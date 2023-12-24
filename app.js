b7b850e2710ead962389843b801e27819143ef1f
aleksaks27 <aleksaks27@gmail.com>
2023-12-24 17:29:49 +0000

    Change from constructor function to class syntax

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const closeButton = document.querySelector("#close");
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read){
