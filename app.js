aad6e3712001cd1e97c71970d70b2c4ba1b10ea6
Aleksandar <aleksaks27@gmail.com>
2024-04-26 20:08:10 +0100

    Save user interactions with localStorage. Add Bootstrap and improve the app's appearance.

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
 
-const myLibrary = [];
+// If the user has interacted with the app before, retrieve their items from localStorage.
+let myLibrary = JSON.parse(window.localStorage.getItem("Library"));
+if (!myLibrary) myLibrary = [];
 
 class Book {
     constructor(title, author, pages, read){
@@ -19,44 +21,49 @@ class Book {
     }
 }
 
-function addBookToLibrary(title, author, pages, read){
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-}
+// Create each new book with the correct HTML elements.
+function displayBooks(index) {
+    while (index < myLibrary.length) {
+        let book = myLibrary[index];
+        let card = document.createElement("div");
+        let cardButtons = document.createElement("div");
+        let deleteButton = document.createElement("button");
+        let toggleButton = document.createElement("button");
+    
+        let title = document.createElement("div");
+        let author = document.createElement("div");
+        let pages = document.createElement("div");
+        title.textContent = `${book.title}`;
+        author.textContent = `${book.author}`;
+        pages.textContent = `${book.pages} pages`;
+    
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete", "btn")
+        deleteButton.addEventListener("click", () => deleteBook(card));
+        toggleButton.textContent = `${book.read}`;
+        toggleButton.classList.add("btn");
+        if(book.read === "Read") {
+            toggleButton.classList.add("btn-success");
+        }
+        else {
+            toggleButton.classList.add("btn-light");
+        }
 
-function displayBooks() {
-    let book = myLibrary[myLibrary.length - 1];
-    let card = document.createElement("div");
-    let deleteButton = document.createElement("div");
-    let toggleButton = document.createElement("div");
+        toggleButton.addEventListener("click", () => toggle(book, toggleButton));
+        cardButtons.setAttribute("id", "card-buttons");
+    
+        card.appendChild(title), card.appendChild(author), card.appendChild(pages);
+        cardButtons.appendChild(toggleButton);
+        cardButtons.appendChild(deleteButton);
+        card.appendChild(cardButtons);
+        display.appendChild(card);
 
-    let title = document.createElement("div");
-    let author = document.createElement("div");
-    let pages = document.createElement("div");
-    title.textContent = `${book.title}`;
-    author.textContent = `${book.author}`;
-    pages.textContent = `${book.pages} pages`;
-
-    deleteButton.textContent = "Delete";
-    deleteButton.setAttribute("class", "delete");
-    deleteButton.addEventListener("click", () => deleteBook(card));
-    toggleButton.textContent = `${book.read}`;
-    if(book.read === "Read") {
-        toggleButton.setAttribute("class", "read");
+        index++
     }
-    else {
-        toggleButton.setAttribute("class", "unread");
-    }
-    toggleButton.addEventListener("click", () => toggle(book, toggleButton));
-
-    card.appendChild(title), card.appendChild(author), card.appendChild(pages);
-    card.appendChild(toggleButton);
-    card.appendChild(deleteButton);
-    display.appendChild(card);
 }
 
 addButton.addEventListener("click", () => {
-    popup.showModal()
+    popup.showModal();
     closeButton.addEventListener("click", () => {
         form.reset();
         popup.close();
@@ -93,7 +100,8 @@ pages.addEventListener("input", () => {
     }
 })
 
-submitButton.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     if (title.value && author.value && pages.value) {
         let title = document.querySelector("#title").value;
         let author = document.querySelector("#author").value;
@@ -102,29 +110,40 @@ submitButton.addEventListener("click", (e) => {
     
         (document.querySelector("#read").checked === true)? read = "Read": read = "Not yet read";
         addBookToLibrary(title, author, pages, read);
-        displayBooks();
+        displayBooks(myLibrary.length - 1);
     
         form.reset();
         popup.close();
-        e.preventDefault();
     }
 })
+
+function addBookToLibrary(title, author, pages, read){
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    window.localStorage.setItem("Library", JSON.stringify(myLibrary));
+}
 
 function deleteBook(card) {
     myLibrary.splice(myLibrary.indexOf(card), 1);
     display.removeChild(card);
+    window.localStorage.setItem("Library", JSON.stringify(myLibrary));
 }
 
 function toggle(book, button) {
     if (book.read === "Read") {
         book.read = "Not yet read";
-        button.removeAttribute("class", "read");
-        button.setAttribute("class", "unread");
+        button.classList.remove("btn-success");
+        button.classList.add("btn-light");
     }
     else {
         book.read = "Read";
-        button.removeAttribute("class", "unread");
-        button.setAttribute("class", "read");
+        button.classList.remove("btn-light");
+        button.classList.add("btn-success");
     }
     button.textContent = book.read;
+    window.localStorage.setItem("Library", JSON.stringify(myLibrary));
 }
+
+// When the app initially loads, render all books in the library. Otherwise render only
+// the latest addition to avoid repeats.
+if (myLibrary.length) displayBooks(0);
